ea41b9300265f1da617cec2c44408f1230ed8a52
ufa <yangbobo0901@gmail.com>
2024-07-29 15:59:12 +0800

    nwaku

--- 1dnode.sh
+++ 1dnode.sh
@@ -6,20 +6,22 @@ function main_menu() {
         clear && logo
         echo "=============项目节点一键部署============"
         echo "请选择项目:"
-        echo "1. 0gAI 一键部署"
-        echo "3. Artela 一键部署"
-        echo "3. Rivalz 一键部署"
+        # echo "1. 0gAI 一键部署"
+        # echo "3. Artela 一键部署"
+        # echo "3. Rivalz 一键部署"
         echo "4. Network3 一键部署"
+        echo "5. waku 一键部署"
         echo "-----------------------其他----------------------"
         echo "0. 退出脚本exit"
         read -p "请输入选项: " OPTION
 
         case $OPTION in
         
-        1) wget -O 0g.sh https://raw.githubusercontent.com/lmooop/lm/main/0g.sh && chmod +x 0g.sh && ./0g.sh ;;
-        2) wget -O Artela.sh https://raw.githubusercontent.com/lmooop/lm/main/Artela.sh && chmod +x Artela.sh && ./Artela.sh ;;
-        3) wget -O rivalz.sh https://raw.githubusercontent.com/lmooop/lm/main/rivalz.sh && chmod +x rivalz.sh && ./rivalz.sh ;;
+        # 1) wget -O 0g.sh https://raw.githubusercontent.com/lmooop/lm/main/0g.sh && chmod +x 0g.sh && ./0g.sh ;;
+        # 2) wget -O Artela.sh https://raw.githubusercontent.com/lmooop/lm/main/Artela.sh && chmod +x Artela.sh && ./Artela.sh ;;
+        # 3) wget -O rivalz.sh https://raw.githubusercontent.com/lmooop/lm/main/rivalz.sh && chmod +x rivalz.sh && ./rivalz.sh ;;
         4) wget -O network3.sh https://raw.githubusercontent.com/lmooop/lm/main/network3.sh && chmod +x network3.sh && ./network3.sh ;;
+        5) wget -O waku.sh https://raw.githubusercontent.com/lmooop/lm/main/waku.sh && chmod +x waku.sh && ./waku.sh ;;
         
         0) echo "退出脚本。"; exit 0 ;;
         *) echo "无效选项，请重新输入。"; sleep 3 ;;
@@ -31,12 +33,11 @@ function main_menu() {
 
 logo()
 {
-echo -e "\033[33m  _    _   _ __  __    _    ___   \033[0m"      
-echo -e "\033[33m | |  | | | |  \/  |  / \  / _ \  \033[0m"      
-echo -e "\033[33m | |  | | | | |\/| | / _ \| | | | \033[0m"      
-echo -e "\033[33m | |__| |_| | |  | |/ ___ \ |_| | \033[0m"      
-echo -e "\033[33m |_____\___/|_|  |_/_/   \_\___/  \033[0m"      
-echo ""
+echo -e '\033[33m      _  __        __    __  ___         __          \033[0m'
+echo -e '\033[33m     / |/ /__  ___/ /__ /  |/  /__ ____ / /____ ____ \033[0m'    
+echo -e '\033[33m    /    / _ \/ _  / -_) /|_/ / _ `(_-</ __/ -_) __/ \033[0m'    
+echo -e '\033[33m   /_/|_/\___/\_,_/\__/_/  /_/\_,_/___/\__/\__/_/    \033[0m'
+echo -e '\033[33m                                                     \033[0m'
 }
 # 显示主菜单
 main_menu
---
+++ nwaku.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# 安装节点函数
+function install_node() {
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install -y curl iptables build-essential git wget jq make gcc nano tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev
+
+    # 检测Docker安装是否成功
+    if ! command -v docker &> /dev/null; then
+        echo "安装 Docker ..."
+        sudo apt install -y docker.io 
+        if ! command -v docker &> /dev/null; then
+            echo "安装 Docker 失败，请检查错误信息。"
+            exit 1
+        fi
+    fi
+
+    # 检查是否需要安装Docker Compose
+    if ! command -v docker-compose &> /dev/null; then
+        echo "安装 Docker Compose ..."
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+        if ! command -v docker-compose &> /dev/null; then
+            echo "安装 Docker Compose 失败，请检查错误信息。"
+            exit 1
+        fi
+    fi
+
+    #rm -rf ./nwaku-compose
+    git clone https://github.com/waku-org/nwaku-compose
+    if [ -d "nwaku-compose" ]; then
+        read -r -p "是否卸载重装，Y 卸载重装(重装前请手动备份)[Y/n] " input
+        case $input in
+            [yY][eE][sS]|[yY])
+        		echo "Yes"
+            rm -rf ./nwaku-compose
+        		;;
+        
+            [nN][oO]|[nN])
+        		echo "No"
+        		  exit 1
+               	;;
+        
+            *)
+        		echo "Invalid input..."
+        		exit 1
+        		;;
+        esac
+    fi
+
+    cp .env.example .env
+    read -r -p "输入infura地址,e.g. https://sepolia.infura.io/v3/123aa110320f4aec179150fba1e1b1b1" RLN_RELAY_ETH_CLIENT_ADDRESS
+    read -r -p "输入钱包私钥,不带0x,e.g. 0116196e9a8abed42dd1a22eb63fa2a5a17b0c27d716b87ded2c54f1bf192a0b" ETH_TESTNET_KEY
+    read -r -p "自定义密码,e.g. 123400800Ib" RLN_RELAY_CRED_PASSWORD
+    sed -i "s#RLN_RELAY_ETH_CLIENT_ADDRESS=.*#RLN_RELAY_ETH_CLIENT_ADDRESS=${RLN_RELAY_ETH_CLIENT_ADDRESS}#g" ~/nwaku-compose/.env
+    sed -i "s#ETH_TESTNET_KEY=.*#ETH_TESTNET_KEY=${ETH_TESTNET_KEY}#g" ~/nwaku-compose/.env
+    sed -i "s#RLN_RELAY_CRED_PASSWORD=.*#RLN_RELAY_CRED_PASSWORD=\"${RLN_RELAY_CRED_PASSWORD}\"#g" ~/nwaku-compose/.env
+
+    docker-compose up -d
+}
+
+
+install_node
