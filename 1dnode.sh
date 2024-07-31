10228f85900d74ebbd14b49c81a3e1d42a1d319f
ufa <yangbobo0901@gmail.com>
2024-07-31 16:42:48 +0800

    nwaku

--- 1dnode.sh
+++ 1dnode.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
 
-# 主菜单
-function main_menu() {
-    while true; do
-        clear && logo
-        echo "=============项目节点一键部署============"
-        echo "请选择项目:"
-        # echo "1. 0gAI 一键部署"
-        # echo "3. Artela 一键部署"
-        # echo "3. Rivalz 一键部署"
-        echo "4. Network3 一键部署"
-        echo "5. waku 一键部署"
-        echo "-----------------------其他----------------------"
-        echo "0. 退出脚本exit"
-        read -p "请输入选项: " OPTION
+PS3="请输入项目编号: "
 
-        case $OPTION in
-        
-        # 1) wget -O 0g.sh https://raw.githubusercontent.com/lmooop/lm/main/0g.sh && chmod +x 0g.sh && ./0g.sh ;;
-        # 2) wget -O Artela.sh https://raw.githubusercontent.com/lmooop/lm/main/Artela.sh && chmod +x Artela.sh && ./Artela.sh ;;
-        # 3) wget -O rivalz.sh https://raw.githubusercontent.com/lmooop/lm/main/rivalz.sh && chmod +x rivalz.sh && ./rivalz.sh ;;
-        4) wget -O network3.sh https://raw.githubusercontent.com/lmooop/lm/main/network3.sh && chmod +x network3.sh && ./network3.sh ;;
-        5) wget -O waku.sh https://raw.githubusercontent.com/lmooop/lm/main/waku.sh && chmod +x waku.sh && ./waku.sh ;;
-        
-        0) echo "退出脚本。"; exit 0 ;;
-        *) echo "无效选项，请重新输入。"; sleep 3 ;;
-        esac
-        echo "按任意键返回主菜单..."
-        read -n 1
-    done
+node_project=(
+  network3 
+  waku
+)
+
+function main_menu() {
+  logo
+  select p in ${node_project[@]}
+  do
+    $p
+    break;
+  done
 }
 
 logo()
@@ -39,5 +24,14 @@ echo -e '\033[33m    /    / _ \/ _  / -_) /|_/ / _ `(_-</ __/ -_) __/ \033[0m'
 echo -e '\033[33m   /_/|_/\___/\_,_/\__/_/  /_/\_,_/___/\__/\__/_/    \033[0m'
 echo -e '\033[33m                                                     \033[0m'
 }
-# 显示主菜单
+
+network3(){
+  wget -O ~/0g.sh https://raw.githubusercontent.com/lmooop/lm/main/network3.sh && chmod +x ~/network3.sh && ~/network3.sh
+}
+
+waku(){
+   wget -O ~/waku.sh https://raw.githubusercontent.com/lmooop/lm/main/waku.sh && chmod +x ~/waku.sh && ~/waku.sh
+}
+
+
 main_menu
