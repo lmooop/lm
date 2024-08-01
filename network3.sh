fcfc1ca7b7e313abd31f87d060ad81413fe73c7d
ufa <yangbobo0901@gmail.com>
2024-08-01 20:20:55 +0800

    comfig

--- network3.sh
+++ network3.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 install ()
 {
+  cd ~
   sudo apt install net-tools -yyq && \
   wget -O ubuntu-node-v2.1.0.tar https://network3.io/ubuntu-node-v2.1.0.tar && \
+  tar -xf ~/ubuntu-node-v2.1.0.tar && \
   cd ~/ubuntu-node && \
   ./manager.sh up && \
   echo -e "\033[31m 请复制44 个字节的私钥 \033[0m" && \
