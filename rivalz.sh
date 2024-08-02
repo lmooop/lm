1294c44b995cb4aa7d7e378d5584778f8da98db2
ufa <yangbobo0901@gmail.com>
2024-08-02 14:30:14 +0800

    add rivalz

--- rivalz.sh
+++ rivalz.sh
@@ -1,22 +1,61 @@
 #!/bin/bash
-if command -v node > /dev/null 2>&1; then
-    echo "Node.js 已安装"
-    npm i pm2@latest -g
-else
-    echo "Node.js 未安装，正在安装..."
-    curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
-    sudo apt-get install -y nodejs
-    npm install pm2@latest -g
-fi
+安装 ()
+{
+  if command -v node > /dev/null 2>&1; then
+      echo "Node.js 已安装"
+      npm i pm2@latest -g
+  else
+      echo "Node.js 未安装，正在安装..."
+      curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+      sudo apt-get install -y nodejs
+      npm install pm2@latest -g
+  fi
+  
+  if command -v npm > /dev/null 2>&1; then
+      echo "npm 已安装"
+  else
+      echo "npm 未安装，正在安装..."
+      sudo apt-get install -y npmGit Graph
+  fi
 
-if command -v npm > /dev/null 2>&1; then
-    echo "npm 已安装"
-else
-    echo "npm 未安装，正在安装..."
-    sudo apt-get install -y npmGit Graph
-fi
-node -v && \
-npm i -g rivalz-node-cli@latest && \
-rivalz run && \
-pm2 start "rivalz run" --name rivalz && pm2 save && pm2 startup && \
-pm2 list
+  node -v && npm i -g rivalz-node-cli@latest && rivalz run && \
+    pm2 start "rivalz run" --name rivalz && \
+    pm2 startup && pm2 save && pm2 list
+  echo "rivalz安装成功..."
+}
+
+添加定时更新版本任务(){
+cat > ~/rivalz_update.sh << EOF
+#!/bin/bash
+/usr/bin/rivalz update-version && pm2 restart rivalz
+EOF
+
+  exist=$(crontab -l|grep -E "rivalz_update")        
+  if [ ! "$run" ] ; then 
+    chmod +x ~/rivalz_update.sh
+    (crontab -l;echo "0 */4 * * * bash ~/rivalz_update.sh") | crontab
+  fi
+}
+options=(
+安装
+添加定时更新版本任务
+)
+
+logo()
+{
+echo -e '\033[33m      _  __        __    __  ___         __          \033[0m'
+echo -e '\033[33m     / |/ /__  ___/ /__ /  |/  /__ ____ / /____ ____ \033[0m'    
+echo -e '\033[33m    /    / _ \/ _  / -_) /|_/ / _ `(_-</ __/ -_) __/ \033[0m'    
+echo -e '\033[33m   /_/|_/\___/\_,_/\__/_/  /_/\_,_/___/\__/\__/_/    \033[0m'
+echo -e '\033[33m                                                     \033[0m'
+}
+menu() {
+  logo
+  PS3="请输入编号: "
+  select p in ${options[@]}
+  do
+    $p
+  done
+}
+
+menu
---
+++ titan.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+安装() {
+    if ! command -v docker &> /dev/null; then
+        echo "安装 Docker ..."
+        sudo apt install -y docker.io 
+        if ! command -v docker &> /dev/null; then
+            echo "安装 Docker 失败，请检查错误信息。"
+            exit 1
+        fi
+        systemctl enable docker
+    fi
+    docker pull nezha123/titan-edge:1.6_amd64
+    mkdir -pv ~/.titanedge/storage
+    read -p $'输入节点 id: \n' id
+    read -p $'分配存储大小,单位G: \n' storage
+
+
+    docker run -d --restart always -v "~/.titanedge/storage:/root/.titanedge/storage" \
+      --net=host \
+      --name "titan" \
+      nezha123/titan-edge:1.6_amd64
+    sleep_time
+    # docker exec $container_id bash -c "\
+    sed -i 's#^[[:space:]]*#StorageGB = .*#StorageGB = $storage_gb#' ~/.titanedge/storage/config.toml
+    sed -i 's#^[[:space:]]*#ListenAddress = \"0.0.0.0:1234\"#ListenAddress = \"0.0.0.0:10086\"#' ~/.titanedge/storage/config.toml
+    docker restart titan && \
+      docker ps -a |grep "titan"
+    docker exec titan bash -c "\
+        titan-edge bind --hash=$id https://api-test1.container1.titannet.io/api/v2/device/binding"
+    echo "titan 安装成功...."
+}
+
+options=(
+安装
+)
+
+sleep_time() {
+    sleep=30
+    while [ $sleep -gt 0 ];do
+      echo -n 等待${sleep}s......
+      sleep 1
+      sleep=$(($sleep - 1))
+      echo -ne "\r     \r"
+    done
+}
+
+logo()
+{
+echo -e '\033[33m      _  __        __    __  ___         __          \033[0m'
+echo -e '\033[33m     / |/ /__  ___/ /__ /  |/  /__ ____ / /____ ____ \033[0m'    
+echo -e '\033[33m    /    / _ \/ _  / -_) /|_/ / _ `(_-</ __/ -_) __/ \033[0m'    
+echo -e '\033[33m   /_/|_/\___/\_,_/\__/_/  /_/\_,_/___/\__/\__/_/    \033[0m'
+echo -e '\033[33m                                                     \033[0m'
+}
+menu() {
+  sleep_time
+  logo
+  PS3="请输入编号: "
+  # logo
+  select p in ${options[@]}
+  do
+    $p
+  done
+}
+menu
